3401541df87f724d5f1470d039056b2f0d82fdee
Bin Wu <binwu@google.com>
2021-02-01 09:52:06 +0000

    fixed a git pulling bug for code updates

--- rust/bin/polkadot_build.sh
+++ rust/bin/polkadot_build.sh
@@ -5,4 +5,5 @@ echo "polkadot_build.sh"
 [ -d polkadot ] || git clone https://github.com/paritytech/polkadot.git; \
     cd polkadot \
     && git checkout $BRANCH \
+    && git pull \
     && cargo build --$PROFILE
