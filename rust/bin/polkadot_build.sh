0ca8b601539118eda81370ec9a0059bbf98ddf61
Bin Wu <binwu@google.com>
2021-02-03 07:37:22 +0000

    minor tweaks

--- rust/bin/polkadot_build.sh
+++ rust/bin/polkadot_build.sh
@@ -4,7 +4,7 @@ echo "polkadot_build.sh"
 
 [ -d polkadot ] || git clone https://github.com/paritytech/polkadot.git; \
     cd polkadot \
-    && git pull \
+    && git pull --all \
     && git checkout $BRANCH \
     && git pull \
     && cargo build --$PROFILE
