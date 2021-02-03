191a5930f011a40faa84c1935c4c02c3e2eea7b6
Bin Wu <binwu@google.com>
2021-02-03 08:03:52 +0000

    pull all branches instead single one

--- rust/bin/polkadot_build.sh
+++ rust/bin/polkadot_build.sh
@@ -4,7 +4,6 @@ echo "polkadot_build.sh"
 
 [ -d polkadot ] || git clone https://github.com/paritytech/polkadot.git; \
     cd polkadot \
-    && git pull --all \
     && git checkout $BRANCH \
     && git pull \
     && cargo build --$PROFILE
